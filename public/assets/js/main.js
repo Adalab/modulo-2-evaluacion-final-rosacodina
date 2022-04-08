3960542c4638c6e779367c4dce36d5f798271fd6
Rosa Codina <rosa.codina90@gmail.com>
2022-04-08 14:34:06 +0200

    Danelis help, html changed and I carried out reviweing the material because I do not understand how to do the next step

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,19 +2,26 @@
 
 console.log('>> Ready :)');
 
-const button = document.querySelector('js__buttonSearch');
-const button = document.querySelector('alert');
+//CONSTANTES
+const button = document.querySelector('.js__buttonSearch');
+const list = document.querySelector('.js__list');
+
+//FUNCIONES
 button.addEventListener('click', (event) => {
-  console.log('event'); //código a ejecutar
+  fetchApi();
 });
 
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita') //aquí le pasamos la URL donde queremos hacer la petición
-  .then(function (response) {
-    //esta es la promesa
-    return response.json();
-  })
-  .then(function (data) {
-    document.body.innerHTML = data.result;
-  });
+function fetchApi() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita') //aquí le pasamos la URL donde queremos hacer la petición
+    .then(function (response) {
+      //esta es la promesa
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.drinks);
+    });
+}
+
+//function to paint in HTML , I have to bring the list to js
 
 //# sourceMappingURL=main.js.map
