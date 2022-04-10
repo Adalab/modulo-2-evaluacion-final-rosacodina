bb7071158fab23eea6405adf8875484cf1aa156f
Rosa Codina <rosa.codina90@gmail.com>
2022-04-11 00:02:25 +0200

    I achieved to pint the list on my website and I deleted it on the HTML

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,6 +6,8 @@ console.log('>> Ready :)');
 const button = document.querySelector('.js__buttonSearch');
 const list = document.querySelector('.js__list');
 
+let cocktails = [];
+
 //FUNCIONES
 button.addEventListener('click', (event) => {
   fetchApi();
@@ -13,15 +15,20 @@ button.addEventListener('click', (event) => {
 
 function fetchApi() {
   fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita') //aquí le pasamos la URL donde queremos hacer la petición
-    .then(function (response) {
-      //esta es la promesa
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.drinks);
+    .then((response) => response.json())
+    .then((data) => {
+      cocktails = data.drinks;
+      console.log(cocktails);
+      createlist();
     });
 }
 
 //function to paint in HTML , I have to bring the list to js
+
+function createlist() {
+  for (const item of cocktails) {
+    list.innerHTML += `<li> ${item.strDrink} </li>`;
+  }
+}
 
 //# sourceMappingURL=main.js.map
